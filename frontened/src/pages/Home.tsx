4cea564a70c873f372e106d91ddf9a5e30b30b64
Deepshika Singh <deepshikasingh110@gmail.com>
2026-02-18 22:48:06 +0530

    fix: contact section scroll on deployed site

--- frontened/src/pages/Home.tsx
+++ frontened/src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Pricing from "../components/Pricing";
@@ -5,17 +7,38 @@ import Faq from "../components/Faq";
 import Contact from "../components/Contact";
 
 export default function Home() {
-    return (
-        <>
-            <Hero />
-            <Features />
-            <Pricing />
-            <section id="faq">
-                <Faq />
-            </section>
-            <section id="contact">
-                <Contact />
-            </section>
-        </>
-    )
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace("#", "");
+
+      // Delay to ensure DOM sections are rendered
+      setTimeout(() => {
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 200);
+    } else {
+      // If no hash and on home, scroll to top
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [location]);
+
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Pricing />
+
+      <section id="faq">
+        <Faq />
+      </section>
+
+      <section id="contact">
+        <Contact />
+      </section>
+    </>
+  );
 }
